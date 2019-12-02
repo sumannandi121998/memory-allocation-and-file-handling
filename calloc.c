ecbccadd7f27def9e52ab1c686373bd0a8821a98
sumannandi121998 <57460338+sumannandi121998@users.noreply.github.com>
2019-12-02 11:08:36 -0800

    finding mean and variance using calloc

---
+++ calloc.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+float *f(float *x,float n,float *arr)
+    {
+    	int i;
+	float s=0,t=0;
+    	for (i=0;i<n;i++)
+	    {
+		  s=s+x[i];
+		  t=t+(x[i]*x[i]);
+	    }
+	    arr[0]=s/n;
+	    arr[1]=(t/n)-(s*s)/(n*n);
+	    return arr;
+    }
+int main()
+{
+	int i;
+	float n=100,arr[100];
+	float *ptr;
+	ptr=(float*)calloc(n,sizeof(float));
+	if (ptr==NULL)
+	{
+		printf("Memory not allocated.\n");
+	}
+	else
+	{
+		printf("Memory successfully allocated.\n");
+	
+	for (i=0;i<n;i++)
+	{
+		ptr[i]=(i+1)*(i+1);
+		printf("%f  ",ptr[i]);
+	}
+	printf("\n");
+
+	float *p=f(ptr,100,arr);
+	printf("The mean is:  %f\nThe variance is:  %f\n",p[0],p[1]);
+	free(ptr);
+        printf("Memory is cleared");
+	
+	}
+	return 0;
+}

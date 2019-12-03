fd6afca2b5073f4759a49847da8aba3f7d1a630d
sumannandi121998 <57460338+sumannandi121998@users.noreply.github.com>
2019-12-03 04:18:45 -0800

    writing the result of computation to a text file

--- write in txt.c
+++ write in txt.c
@@ -3,16 +3,16 @@
 int main()
 {
 	FILE *ptr;
-	ptr=fopen("file.txt","w");
+	ptr=fopen("file.txt","w"); //creating the txt file
 	if (ptr==NULL)
 	{
-		printf("File is not open");
+	   printf("File is not open");
 	}
 	else
-	  {
+	 {
 	    printf("file is open\n");
-	      }
-	fprintf(ptr,"%s\n%s","The mean is: 3383.5","The variance is: 9055261.0");
-	fclose(ptr);
+	  }
+	fprintf(ptr,"%s\n%s","The mean of the numbers is: 3383.50","The variance of the numbers is: 9055261.05"); //writing in txt file
+	fclose(ptr); //close the txt file
 	return 0;
 }

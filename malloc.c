a18bb79115c76c5dd422a5cdc884f8a637018cc3
sumannandi121998 <57460338+sumannandi121998@users.noreply.github.com>
2019-12-03 04:03:32 -0800

    Finding mean and variance using malloc

--- malloc.c
+++ malloc.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-float *f(float *x,float n,float *arr)
+float *f(float *x,float n,float *arr) //function for finding mean and variance
     {
     	int i;
 	float s=0,t=0;
@@ -19,25 +19,24 @@ int main()
 	int i;
 	float n=100,arr[100];
 	float *ptr;
-	ptr=(float*)malloc(n*sizeof(float));
+	ptr=(float*)malloc(n*sizeof(float)); //memory allocation using malloc
 	if (ptr==NULL)
 	{
-		printf("Memory not allocated.\n");
+		printf("Memory is not allocated.\n");
 	}
 	else
 	{
-		printf("Memory successfully allocated.\n");
+		printf("Memory is successfully allocated.\n");
 	
 	for (i=0;i<n;i++)
 	{
 		ptr[i]=(i+1)*(i+1);
 	}
 
-	float *p=f(ptr,100,arr);
-	printf("The mean is:  %f\nThe variance is:  %f\n",p[0],p[1]);
-	free(ptr);
+	float *p=f(ptr,100,arr); //function call
+	printf("The mean of the numbers is:  %f\nThe variance of the numbers is:  %f\n",p[0],p[1]);
+	free(ptr); //malloc memory clear
         printf("Memory is cleared");
-	
 	}
 	return 0;
 }

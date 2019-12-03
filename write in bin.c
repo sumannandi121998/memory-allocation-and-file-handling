2bd13b1584add2abcc6d3703944871f1ebd221cf
sumannandi121998 <57460338+sumannandi121998@users.noreply.github.com>
2019-12-03 04:22:21 -0800

    writing the result of computation to a binary file

--- write in bin.c
+++ write in bin.c
@@ -3,17 +3,17 @@
 int main()
 {
 	FILE *ptr;
-	char ch[]="The mean is: 3383.5\nThe variance is: 9055261.0";
-	ptr=fopen("file2.bin","wb");
+	char ch[]="The mean of the numbers is: 3383.50\nThe variance of the numbers is: 9055261.05";
+	ptr=fopen("file2.bin","wb"); //creating the binary file
 	if (ptr==NULL)
 	{
-		printf("Error");
+	    printf("File is not open");
 	}
 	else
-	  {
+	 {
 	    printf("file is open\n");
-	      }
-	fwrite(ch,sizeof(ch),1,ptr);
-	fclose(ptr);
+	 }
+	fwrite(ch,sizeof(ch),1,ptr); //writing in binary file
+	fclose(ptr); //close the binary file
 	return 0;
 }

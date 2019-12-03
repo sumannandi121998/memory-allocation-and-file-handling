6486cf9e3210e3177f76d866ec877108d2acbac5
sumannandi121998 <57460338+sumannandi121998@users.noreply.github.com>
2019-12-03 04:31:13 -0800

    calculating mean and variance using GSL function

--- gsl.c
+++ gsl.c
@@ -1,14 +1,14 @@
-#include <gsl/gsl_statistics_float.h>
+#include <gsl/gsl_statistics_float.h> //including GSL library
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
-#include "function.h"
+#include "function.h" //including function for calculating mean and variance
 
 int main()
 {
 int i;
 float m[2];
-float* a=(float*)malloc(100*sizeof(float));
+float* a=(float*)malloc(100*sizeof(float)); //malloc memory allocation
 float mean,var,m2,v2;
 for(i=0;i<100;i++)
 {
@@ -16,12 +16,12 @@ a[i]=(i+1)*(i+1);
 }
 m[0]=*f(a,100,arr);
 m[1]=*(f(a,100,arr)+1);
-m2=gsl_stats_float_mean(a,1,100);
-v2=gsl_stats_float_variance(a,1,100);  /*variance and mean from gsl library*/
-printf("The mean is(by loop):%f\n",m[0]);
-printf("The mean is(by gsl):%f\n",m2);
-printf("The variance is:%f\n(by loop)",m[1]);
-printf("The variance is:%f\n(by gsl)",v2);
+m2=gsl_stats_float_mean(a,1,100); //mean from gsl library
+v2=gsl_stats_float_variance(a,1,100);  //variance from gsl library
+printf("The mean of the numbers is(by loop):%f\n",m[0]);
+printf("The mean of the numbers is(by gsl):%f\n",m2);
+printf("The variance of the numbers is(by loop):%f\n",m[1]);
+printf("The variance of the numbers is(by loop):%f\n",v2);
 return(0);
-free(a);
+free(a); //memory cleared
 }

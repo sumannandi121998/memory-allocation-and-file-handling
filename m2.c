dc71690c94b12bed93fe9114fe4bc3dc57d13733
sumannandi121998 <57460338+sumannandi121998@users.noreply.github.com>
2019-12-03 04:10:31 -0800

    another file that include function.h header file

--- m2.c
+++ m2.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "function.h"
+#include "function.h" //including header file
 int main()
 {
 	int i;
@@ -9,22 +9,21 @@ int main()
 	ptr=(float*)malloc(n*sizeof(float));
 	if (ptr==NULL)
 	{
-		printf("Memory not allocated.\n");
+		printf("Memory is not allocated.\n");
 	}
 	else
 	{
-		printf("Memory successfully allocated.\n");
+		printf("Memory is successfully allocated.\n");
 	
 	for (i=0;i<n;i++)
 	{
 		ptr[i]=(i+1)*(i+1);
 	}
 
-	float *p=f(ptr,100,arr);
-	printf("The mean is:  %f\nThe variance is:  %f\n",p[0],p[1]);
-	free(ptr);
+	float *p=f(ptr,100,arr); //function call
+	printf("The mean of the numbers is:  %f\nThe variance of the numbers is:  %f\n",p[0],p[1]);
+	free(ptr); //malloc memory clear
         printf("Memory is cleared");
-	
 	}
 	return 0;
 }
